9a09c8bcef1c12b52063fa9109eee87fa4571035
Mikhail <misha170801planshet@gmail.com>
2024-06-15 14:29:51 +0300

    Extracting all required info to string

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ and filtering all previous entries with less than or equal to five words in the 
 
 url = 'https://news.ycombinator.com/'
 response = requests.get(url)
+
 soup = BeautifulSoup(response.text, 'html.parser')
+
 news_list = soup.find_all('tr', class_='athing')
-for news in news_list:
+subtext_list = soup.find_all('td', class_='subtext')
+
+for index, (news, subtext) in enumerate(zip(news_list, subtext_list)):
     news_number = news.find('span', class_='rank').get_text()
-    news_title = news.find('span', class_='titleline').get_text()
-    # news_points = news.find('span', class_='').get_text()
-    # news_comments = news.find('span', class_='').get_text()
-    print(news_number, news_title)
+    news_title = news.find('span', class_='titleline').find('a').get_text()
+    news_points = subtext.find('span', class_='score').get_text()
+    news_comments = subtext.find_all('a')[-1].get_text()
+    print(news_number, news_title, news_points, news_comments)
+
+
